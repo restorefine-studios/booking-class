9d64681c39b0652a0782cbf621db5bb834b5cd80
prabish01 <prabishdangi2002@gmail.com>
2025-11-17 23:05:07 +0545

    nxt image opt off

--- frontend/next.config.ts
+++ frontend/next.config.ts
@@ -2,6 +2,7 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   images: {
+    unoptimized: true, // Disable Next.js image optimization
     remotePatterns: [
       {
         protocol: "http",
